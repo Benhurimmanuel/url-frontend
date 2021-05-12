3632347cb9593d4993786e4b488f9cd102644fc6
Benhurimmanuel <benhurimmanuel@gmail.com>
2021-05-13 00:17:10 +0530

    api changed

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export function PostUrl(id, longUrl) {
-  return axios.post(`http://localhost:8080/urlshortner/${id}`, longUrl, {
+  return axios.post(`https://myurlapp.herokuapp.com/urlshortner/${id}`, longUrl, {
     headers: {
       authorization: window.localStorage.getItem("app_token"),
     },
   });
 }
 export function PostUserRegister(userData) {
-  return axios.post("http://localhost:8080/register", userData);
+  return axios.post("https://myurlapp.herokuapp.com/register", userData);
 }
 
 export function PostLogin(loginData) {
-  return axios.post("http://localhost:8080/login", loginData);
+  return axios.post("https://myurlapp.herokuapp.com/login", loginData);
 }
 export function getUrl(id) {
-  return axios.get(`http://localhost:8080/urlshortner/${id}`, {
+  return axios.get(`https://myurlapp.herokuapp.com/urlshortner/${id}`, {
     headers: {
       authorization: window.localStorage.getItem("app_token"),
     },
